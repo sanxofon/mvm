cd62788375bcc723e1553d76cb9d4a7eebcb08ad
Santiago Chávez <sanxofon@users.noreply.github.com>
2022-06-03 14:55:22 -0300

    Add template PWA
    Se agregó la primera versión de la plantilla para Progressive Web Apps del Museo Virtual de Matemáticas

---
+++ app/appConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+$app_name = "Progressive Web App Template";
+$app_shortname = "PWAT2022";
+$app_author = "Santiago C. Novaro";
+$app_authorTwitter = "@sanxofon";
+$app_version = "v0.5";
+$app_year = "2022";
+if(@$_GET['launcher']) $app_version = $app_version."b";
+$app_description = "Máxima compatibilidad con los nuevos dispositivos inteligentes Android";
+$app_server = "//localhost";
+$app_path = "/app/";
