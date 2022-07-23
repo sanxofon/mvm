4a005f9ffe69c3764c1c26c6de071c9ff38a0274
Santiago Chávez <sanxofon@users.noreply.github.com>
2022-07-23 11:54:31 -0500

    update data.js

--- salas/data.js
+++ salas/data.js
@@ -94,6 +94,37 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
+      "id": "3-hormiguero-sideral-02",
+      "name": "hormiguero-sideral-02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
       "id": "4-mezcla_cam",
       "name": "mezcla_cam",
       "levels": [
