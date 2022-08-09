ec84afddce849078c732432bf63ff2f973d1247e
Santiago Chávez <sanxofon@users.noreply.github.com>
2022-08-09 18:28:02 -0300

    update salas barco

--- salas/data.js
+++ salas/data.js
@@ -156,7 +156,38 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "5-gato-3000x1500",
+      "id": "5-barco-3",
+      "name": "Barco-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-gato-3000x1500",
       "name": "gato-3000x1500",
       "levels": [
         {
